3a944c77613194a8892bf01fefb85f93b90099fc
Mark Hutchison <mark.d.hutchison@gmail.com>
2013-09-15 15:49:31 -0700

    initial commit

---
+++ FirstDialog/src/firstdialog/FirstDialog.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstdialog;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author nsmarine
+ */
+public class FirstDialog {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Welcome!");
+    }
+}
