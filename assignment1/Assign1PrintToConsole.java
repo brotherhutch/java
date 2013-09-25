ac0e8b8e102cee6a2884bf845128718408b39278
Mark Hutchison <mark.d.hutchison@gmail.com>
2013-09-24 21:03:28 -0700

    Basic hello world

---
+++ assignment1/Assign1PrintToConsole.java
@@ -0,0 +1,18 @@
+
+package assign1.print.to.console;
+
+/**
+ *
+ * @author nsmarine
+ */
+public class Assign1PrintToConsole {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Welcome to Java, Welcome to Computer Science, Programming is fun.");
+        System.exit(0);
+    }
+}
